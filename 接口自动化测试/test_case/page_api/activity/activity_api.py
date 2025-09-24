7ba96a03ef18583f5621d8015332af476df73be9
user <bd22434@163.com>
2025-09-24 22:41:52 +0800

    1.9接口覆盖-9.24.13

--- 接口自动化测试/test_case/page_api/activity/activity_api.py
+++ 接口自动化测试/test_case/page_api/activity/activity_api.py
@@ -78,3 +78,27 @@ class ActivityApi(BaseAPI):
             response = response.json()
             return response
 
+    def draw(self, authorization, activityId=720712510713925, kidId=0, DeviceType="web", code=200, **kwargs):
+        """
+        抽奖
+        :param activityId: (integer, query, required) activityId
+        :param kidId: (integer, query, required) kidId
+        :return: 接口原始返回（已 json 解析）
+        """
+        # Create Data:  V1.19.0  &  2025-09-24
+        url = f"https://{base_url}/api/activity/gacha/draw"
+        payload = {
+            "activityId": activityId,
+            "kidId": kidId
+        }
+        payload = self.request_body(payload, **kwargs)
+        timestamp = str(int(time.time() * 1000))
+        headers = self.request_header(timestamp, authorization, DeviceType)
+
+        response = requests.request("POST", url, headers=headers, params=payload)
+        error_msg = "抽奖"
+        assert response.status_code == code, f"{error_msg}失败，url->{url}，失败信息->{response.reason}{response.content}"
+        if response.status_code != 401:
+            response = response.json()
+            return response
+
