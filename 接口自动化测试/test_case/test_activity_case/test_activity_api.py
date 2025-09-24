9dfeb1da36acd608e237d0cb8985f7facc258e00
user <bd22434@163.com>
2025-09-24 22:54:02 +0800

    1.9接口覆盖-9.24.14

--- 接口自动化测试/test_case/test_activity_case/test_activity_api.py
+++ 接口自动化测试/test_case/test_activity_case/test_activity_api.py
@@ -454,7 +454,7 @@ class TestActivity:
             ('invalid_token', 'invalid_token'),
         ]
     )
-    def test_activity_permission_getInfo1(self, desc, value, getkidId):
+    def test_activity_permission_draw(self, desc, value, getkidId):
         """抽奖-{desc}"""
         kidId = getkidId[0]["id"]
         # 鉴权作为位置参数直接传入（示例期望的极简风格）
@@ -622,3 +622,194 @@ class TestActivity:
         assert res['code'] == 100105
         assert res['message'] == 'Kid id not exist'
         assert res['data'] == 'Kid id not exist'
+
+    @pytest.mark.release
+    def test_activity_positive_getRewardcenter_ok(self, getkidId):
+        """扭蛋奖励中心-正向用例"""
+        kidId = getkidId[0]["id"]
+        res = self.activity.getRewardcenter(self.authorization, kidId=kidId)
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        assert res['code'] == 200
+        assert res['message'] == 'success'
+        assert res['data']
+
+    @pytest.mark.release
+    @pytest.mark.parametrize(
+        'desc, value',
+        [
+            ('unauthorized', 'missing'),
+            ('no_auth', ''),
+            ('expired_token', 'expired_token'),
+            ('invalid_token', 'invalid_token'),
+        ]
+    )
+    def test_activity_permission_getRewardcenter(self, desc, value, getkidId):
+        """扭蛋奖励中心-{desc}"""
+        kidId = getkidId[0]["id"]
+        # 鉴权作为位置参数直接传入（示例期望的极简风格）
+        res = self.activity.getRewardcenter(value, kidId=kidId, code=401)
+
+    @pytest.mark.release
+    @pytest.mark.parametrize(
+        'desc, value, code',
+        [
+            ('missing', 'missing', 500),
+            ('empty', "''", 400),
+            ('null', None, 500),
+        ]
+    )
+    def test_activity_required_getRewardcenter_activityId(self, desc, value, code, getkidId):
+        """扭蛋奖励中心-必填字段测试-{desc}(activityId)"""
+        kidId = getkidId[0]["id"]
+        if desc == 'missing':
+            pl, activityId = {'pop_items': 'activityId'}, 1
+        else:
+            pl, activityId, kidId = {}, value, kidId
+        res = self.activity.getRewardcenter(self.authorization, kidId=kidId, activityId=activityId, code=code, **pl)
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        if code == 500:
+            assert res['code'] == code
+            assert res['message'] == 'internal server error'
+            assert res['data'][
+                       'message'] == "Required request parameter 'activityId' for method parameter type long is not present"
+        else:
+            assert res['code'] == 100006
+            assert res['message'] == 'invalid parameter'
+            assert res[
+                       'data'] == '''Failed to convert value of type 'java.lang.String' to required type 'long'; nested exception is java.lang.NumberFormatException: For input string: "''"'''
+
+    @pytest.mark.release
+    @pytest.mark.parametrize(
+        'desc, value',
+        [
+            ('string', '"abc"'),
+            ('float', 12.34),
+            ('boolean', True),
+            ('array', [1, 2, 3]),  # TODO
+            ('object', {"key": "value"}),
+            ('special_chars', '!@#$%^&*()'),
+            ('emoji', '���'),
+            ('long_string',
+             '"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"'),
+        ]
+    )
+    def test_activity_format_getRewardcenter_activityId(self, desc, value, getkidId):
+        """扭蛋奖励中心-数据格式测试-{desc}(activityId)"""
+        kidId = getkidId[0]["id"]
+        code = 400 if desc not in ['special_chars', 'emoji'] else 403
+        res = self.activity.getRewardcenter(self.authorization, kidId=kidId, activityId=value, code=code)
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        if desc not in ['special_chars', 'emoji']:
+            assert res['code'] == 100006
+            assert res['message'] == 'invalid parameter'
+            assert '''Failed to convert value of type 'java.lang.String' to required type 'long'; ''' in res['data']
+        else:
+            assert res['code'] == 100114
+            assert res['message'] == 'Activity not found'
+            assert res['data'] == 'Activity not found'
+
+    @pytest.mark.release
+    @pytest.mark.parametrize(
+        'desc, value',
+        [
+            ('min', -2147483648),  # TODO -- 这个更加特殊，都没显示不存在，尽然调通了
+            ('zero', 0),
+            ('max', 2147483647),
+        ]
+    )
+    def test_activity_boundary_getRewardcenter_activityId(self, desc, value, getkidId):
+        """扭蛋奖励中心-边界值测试-{desc}(activityId)"""
+        kidId = getkidId[0]["id"]
+        res = self.activity.getRewardcenter(self.authorization, kidId=kidId, activityId=value)
+        assert res['code'] == 100114
+        assert res['message'] == 'Activity not found'
+        assert res['data'] == 'Activity not found'
+
+    @pytest.mark.release
+    def test_activity_scenario_getRewardcenter_invalid_activityId(self, getkidId):
+        """扭蛋奖励中心-场景异常-无效的activityId"""
+        kidId = getkidId[0]["id"]
+        res = self.activity.getRewardcenter(self.authorization, kidId=kidId, activityId=99999)  # TODO
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        assert res['code'] == 100114
+        assert res['message'] == 'Activity not found'
+        assert res['data'] == 'Activity not found'
+
+    @pytest.mark.release
+    @pytest.mark.parametrize(
+        'desc, value, code',
+        [
+            ('missing', 'missing', 500),
+            ('empty', "''", 400),
+            ('null', None, 500),
+        ]
+    )
+    def test_activity_required_getRewardcenter_kidId(self, desc, value, code, create_activity):
+        """扭蛋奖励中心-必填字段测试-{desc}(kidId)"""
+        if desc == 'missing':
+            pl, kidId = {'pop_items': 'kidId'}, 0
+        else:
+            pl, kidId = {}, value
+        res = self.activity.getRewardcenter(self.authorization, kidId=kidId, code=code, **pl)
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        if code == 500:
+            assert res['code'] == code
+            assert res['message'] == 'internal server error'
+            assert res['data'][
+                       'message'] == "Required request parameter 'kidId' for method parameter type long is not present"
+        else:
+            assert res['code'] == 100006
+            assert res['message'] == 'invalid parameter'
+            assert res[
+                       'data'] == '''Failed to convert value of type 'java.lang.String' to required type 'long'; nested exception is java.lang.NumberFormatException: For input string: "''"'''
+
+    @pytest.mark.release
+    @pytest.mark.parametrize(
+        'desc, value',
+        [
+            ('string', 'abc'),
+            ('float', 12.34),
+            ('boolean', True),
+            ('array', [1, 2, 3]),  # TODO
+            ('object', {"key": "value"}),
+            ('special_chars', '!@#$%^&*()'),  # TODO
+            ('emoji', '���'),  # TODO
+            ('long_string',
+             'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+        ]
+    )
+    def test_activity_format_getRewardcenter_kidId(self, desc, value):
+        """扭蛋奖励中心-数据格式测试-{desc}(kidId)"""
+        res = self.activity.getRewardcenter(self.authorization, kidId=value, code=400)
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        assert res['code'] == 100006
+        assert res['message'] == 'invalid parameter'
+        assert '''Failed to convert value of type 'java.lang.String' to required type 'long'; nested exception is java.lang.NumberFormatException: For input string: ''' in \
+               res['data']
+
+    @pytest.mark.release
+    @pytest.mark.parametrize(
+        'desc, value',
+        [
+            ('min', -2147483648),  # TODO
+            ('zero', 0),
+            ('max', 2147483647),
+        ]
+    )
+    def test_activity_boundary_getRewardcenter_kidId(self, desc, value):
+        """扭蛋奖励中心-边界值测试-{desc}(kidId)"""
+        res = self.activity.getRewardcenter(self.authorization, kidId=value)
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        assert res['code'] == 100105
+        assert res['message'] == 'Kid id not exist'
+        assert res['data'] == 'Kid id not exist'
+
+    @pytest.mark.release
+    def test_activity_scenario_getRewardcenter_invalid_kidId(self):
+        """扭蛋奖励中心-场景异常-无效的kidId"""
+        res = self.activity.getRewardcenter(authorization=self.authorization, kidId=99999999)
+        assert res['code'] == 100105
+        assert res['message'] == 'Kid id not exist'
+        assert res['data'] == 'Kid id not exist'
+
+
