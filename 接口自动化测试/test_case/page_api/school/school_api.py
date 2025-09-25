dede8075be3d0f5afa433bd5f1d2d0b3e4b96530
Baidi <baidi@qq.com>
2025-09-25 11:15:55 +0800

    接口框架维护9.25

---
+++ 接口自动化测试/test_case/page_api/school/school_api.py
@@ -0,0 +1,38 @@
+
+import time
+
+from test_case.page_api.base_api import BaseAPI
+
+requests = BaseAPI().http_timeout()
+base_url = BaseAPI().baseurl()
+
+
+class SchoolApi(BaseAPI):
+    """学校系统接口"""
+
+    def schoolClass(self, authorization, DeviceType="web", code=200, **kwargs):
+        """
+        创建班级
+        :param className: (string, body, required) className 参数
+        :param description: (string, body, required) description 参数
+        :param grade: (integer, body, required) grade 参数
+        :param subject: (string, body, required) subject 参数
+        :return: 接口原始返回（已 json 解析）
+        """
+        # Create Data:  V1.19.0  &  2025-09-25
+        url = f"https://{base_url}/api/school/class"
+        payload = {
+            "className": "三年级一班",
+            "description": "三年级英语学习班",
+            "grade": 3,
+            "subject": "English"
+        }
+        timestamp = str(int(time.time() * 1000))
+        headers = self.request_header(timestamp, authorization, DeviceType)
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+        error_msg = "创建班级"
+        assert response.status_code == code, f"{error_msg}失败，url->{url}，失败信息->{response.reason}{response.content}"
+        response = response.json()
+        return response
+
