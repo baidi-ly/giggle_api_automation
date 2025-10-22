03a1d776aad219db3bca20158d8c8cd8eb248e07
user <bd22434@163.com>
2025-10-22 16:56:53 +0800

    v20接口覆盖10.22.3

---
+++ 接口自动化测试/test_case/test_admin_case/test_admin_learningstats_api.py
@@ -0,0 +1,26 @@
+
+import sys
+import os
+
+from test_case.page_api.admin.admin_learning_api import AdminLearningApi
+
+sys.path.append(os.getcwd())
+sys.path.append("..")
+
+import pytest
+
+@pytest.mark.Admin
+@pytest.mark.AdminFlashcard
+class TestAdminFlashcard:
+
+    def setup_class(self):
+        self.admin_learning = AdminLearningApi()
+        self.authorization = self.admin_learning.get_authorization()[0]
+
+    def test_admin_learningstats_positive_ok(self):
+        """创建学习计划（包含单元和内容）-正向用例"""
+        res = self.admin_learning.trigger_weekly_reports(self.authorization)
+        assert isinstance(res, dict), f'接口返回类型异常: {type(res)}'
+        assert res['code'] == 200, f"接口返回状态码异常: 预期【200】，实际【{res['code']}】"
+        assert res['message'] == 'success', f"接口返回message信息异常: 预期【success】，实际【{res['message']}】"
+        assert res['data'], f"接口返回data数据异常：{res['data']}"
